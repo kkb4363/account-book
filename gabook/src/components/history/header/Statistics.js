1f907a18ca5e1f6972371c9c4e8076fb4c65a01d
Gibeom <101088024+kkb4363@users.noreply.github.com>
2023-12-04 20:58:01 +0900

    refactor: history, main pages (#1)
    * feat: history 페이지 단일책임원칙
    
    * chore: main 변수 명 수정

---
+++ gabook/src/components/history/header/Statistics.js
@@ -0,0 +1,61 @@
+import styled from "styled-components";
+import utils from "../../../utils/utils";
+
+export default function HistoryStatistics(props) {
+  const { costFormatter } = utils();
+  const expenses = props.currentDateHistories?.reduce((acc, cur) => {
+    const isExpenses = cur.type == "지출";
+    if (isExpenses) {
+      const costWithoutComma = cur.cost.replace(/,/g, "");
+      acc += Number(costWithoutComma);
+    }
+    return acc;
+  }, 0);
+
+  const income = props.currentDateHistories?.reduce((acc, cur) => {
+    const isIncome = cur.type == "수입";
+    if (isIncome) {
+      const costWithoutComma = cur.cost.replace(/,/g, "");
+      acc += Number(costWithoutComma);
+    }
+    return acc;
+  }, 0);
+  const totalCost = () => {
+    const isMinus = income - expenses < 0;
+    if (!isMinus) return "+" + costFormatter(income - expenses);
+    else return "-" + costFormatter(income - expenses);
+  };
+
+  return (
+    <HistoryStatisticsRow>
+      <span>{props.current.month}월 내 소비</span>
+      <span>수입 : {"+" + costFormatter(income)}원</span>
+      <span>지출 : {"-" + costFormatter(expenses)}원</span>
+      <span>합계 : {totalCost()}원</span>
+    </HistoryStatisticsRow>
+  );
+}
+
+const HistoryStatisticsRow = styled.div`
+  width: 100vw;
+  height: 20%;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  margin: 0 0 -2.5rem -2rem;
+  background-color: white;
+
+  font-size: ${({ theme }) => theme.fontsize.sm};
+  font-weight: ${({ theme }) => theme.weight.lg};
+  color: black;
+
+  span:first-child {
+    color: gray;
+  }
+  span:nth-child(2) {
+    color: ${({ theme }) => theme.colors.blue};
+  }
+  span:nth-child(3) {
+    color: ${({ theme }) => theme.colors.error};
+  }
+`;
--- gabook/src/pages/History.js
+++ gabook/src/pages/History.js
@@ -1,6 +1,4 @@
 import React, { useState } from "react";
-import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
-import { useNavigate } from "react-router-dom";
 import { useRecoilValue, useSetRecoilState } from "recoil";
 import { keyframes, styled } from "styled-components";
 import { currentCategoryAtom } from "../atoms/CategoryAtom";
@@ -10,18 +8,16 @@ import CategoryUpdate from "../components/category/CategoryUpdate";
 import PlusIcon from "../components/common/PlusIcon";
 import HistoryItem from "../components/history/HistoryItem";
 import HistoryUpdate from "../components/history/HistoryUpdate";
+import HistoryHeader from "../components/history/header/Header";
 import MotionInputs from "../components/motion/MotionInput";
 import useAddHistory from "../hooks/useAddHistory";
 import UseHandler from "../hooks/useHandler";
 import { flexColumn, fullScreen } from "../styled/styled";
-import utils from "../utils/utils";
 
 const History = () => {
-  const setCurrentCategory = useSetRecoilState(currentCategoryAtom);
   const { open: openAdd, handleToggle: handleAdd, openItems } = useAddHistory();
-  const { costFormatter } = utils();
+  const setCurrentCategory = useSetRecoilState(currentCategoryAtom);
   const history = useRecoilValue(historyAtom);
-  const navigate = useNavigate();
   const date = new Date();
   const fields = ["category", "addCategory"];
   const [open, closeAll, handleToggle] = UseHandler(fields);
@@ -33,29 +29,15 @@ const History = () => {
     month: date.getMonth() + 1,
     year: date.getFullYear(),
   });
-  const prevMonth = current.month - 1 == 0 ? 12 : current.month - 1;
-  const nextMonth = current.month + 1 == 13 ? 1 : current.month + 1;
   const currentDate = current.year + "" + current.month;
 
-  const onPrevMonth = () => {
-    const prevmon = current.month - 1 == 0 ? 12 : current.month - 1;
-    const prevYear = prevmon === 12 ? current.year - 1 : current.year;
-    setCurrent({
-      month: prevmon,
-      year: prevYear,
-    });
-  };
-  const onNextMonth = () => {
-    const nextmon = current.month + 1 == 13 ? 1 : current.month + 1;
-    const nextYear = nextmon === 1 ? current.year + 1 : current.year;
-    setCurrent({ month: nextmon, year: nextYear });
-  };
   const openEditHandler = (sid) => {
     setEdit({
       open: true,
       id: sid,
     });
   };
+
   const closeEditHandler = () => {
     setEdit({
       open: false,
@@ -66,36 +48,13 @@ const History = () => {
       text: "",
     });
   };
+
   const openCategory = () => handleToggle("category");
 
   const currentMonthDatas = history?.filter((item) => {
     const checkValue = item?.date?.slice(0, 4) + item?.date?.slice(5, 7);
     return checkValue == currentDate;
   });
-
-  const expenses = currentMonthDatas?.reduce((acc, cur) => {
-    const isExpenses = cur.type == "지출";
-    if (isExpenses) {
-      const costWithoutComma = cur.cost.replace(/,/g, "");
-      acc += Number(costWithoutComma);
-    }
-    return acc;
-  }, 0);
-
-  const income = currentMonthDatas?.reduce((acc, cur) => {
-    const isIncome = cur.type == "수입";
-    if (isIncome) {
-      const costWithoutComma = cur.cost.replace(/,/g, "");
-      acc += Number(costWithoutComma);
-    }
-    return acc;
-  }, 0);
-
-  const totalCost = () => {
-    const isMinus = income - expenses < 0;
-    if (!isMinus) return "+" + costFormatter(income - expenses);
-    else return "-" + costFormatter(income - expenses);
-  };
 
   const groupedData = currentMonthDatas.reduce((groups, item) => {
     const date = new Date(item.date);
@@ -134,35 +93,14 @@ const History = () => {
     ));
 
   return (
-    <HistoryWrapper>
-      <HistoryHeader>
-        <div onClick={() => navigate("/")}>
-          <AiOutlineLeft />
-        </div>
-
-        <HeaderDate>
-          <span>{prevMonth}</span>
-          <span onClick={onPrevMonth}>
-            <AiOutlineLeft />
-          </span>
-          <span>{current.month}</span>
-          <span onClick={onNextMonth}>
-            <AiOutlineRight />
-          </span>
-          <span>{nextMonth}</span>
-        </HeaderDate>
-
-        <HeaderTotal>
-          <span>{current.month}월 내 소비</span>
-          <span>수입 : {"+" + costFormatter(income)}원</span>
-          <span>지출 : {"-" + costFormatter(expenses)}원</span>
-          <span>합계 : {totalCost()}원</span>
-        </HeaderTotal>
-      </HistoryHeader>
-
-      <CurrentText>최근 내역</CurrentText>
-      <CurrentHistory>{result}</CurrentHistory>
-
+    <HistoryLayout>
+      <HistoryHeader
+        currentDateHistories={currentMonthDatas}
+        current={current}
+        setCurrent={setCurrent}
+      />
+      <HistoryTitle>최근 내역</HistoryTitle>
+      <HistoryCurrentCol>{result}</HistoryCurrentCol>
       <PlusIcon onClick={() => handleAdd("addMoney")} />
 
       {openItems.map(
@@ -196,7 +134,7 @@ const History = () => {
           <CategoryUpdate onAddCate={() => handleToggle("addCategory")} />
         </MotionInputs>
       )}
-    </HistoryWrapper>
+    </HistoryLayout>
   );
 };
 
@@ -211,79 +149,13 @@ const HistoryKeyframes = keyframes`
   }
 `;
 
-const HistoryWrapper = styled.div`
+const HistoryLayout = styled.div`
   ${fullScreen};
   ${flexColumn};
   animation: ${HistoryKeyframes} 0.5s ease-in-out;
 `;
 
-const HistoryHeader = styled.div`
-  width: 100%;
-  height: 30%;
-  ${flexColumn};
-  justify-content: space-evenly;
-  background: #3f3e3e;
-  box-sizing: border-box;
-  padding: 0 2rem 0 2rem;
-
-  div:first-child {
-    cursor: pointer;
-
-    color: white;
-    font-size: ${({ theme }) => theme.fontsize.xxxl};
-  }
-`;
-
-const HeaderDate = styled.div`
-  width: 100%;
-  height: 25%;
-  display: flex;
-  align-items: flex-start;
-  justify-content: space-around;
-
-  font-size: 2rem;
-  font-weight: ${({ theme }) => theme.weight.lg};
-  color: gray;
-
-  span:nth-child(3) {
-    color: white;
-  }
-
-  span:nth-child(2),
-  span:nth-child(4) {
-    cursor: pointer;
-  }
-
-  @media screen and (min-width: 1000px) {
-    margin: 0 auto;
-    width: 50%;
-  }
-`;
-
-const HeaderTotal = styled.div`
-  width: 100vw;
-  height: 20%;
-  display: flex;
-  align-items: center;
-  justify-content: space-around;
-  margin: 0 0 -2.5rem -2rem;
-
-  font-size: ${({ theme }) => theme.fontsize.sm};
-  font-weight: ${({ theme }) => theme.weight.lg};
-  color: black;
-  background-color: white;
-  span:first-child {
-    color: gray;
-  }
-  span:nth-child(2) {
-    color: ${({ theme }) => theme.colors.blue};
-  }
-  span:nth-child(3) {
-    color: ${({ theme }) => theme.colors.error};
-  }
-`;
-
-const CurrentHistory = styled.div`
+const HistoryCurrentCol = styled.div`
   width: 100%;
   height: 50%;
   ${flexColumn};
@@ -297,7 +169,7 @@ const CurrentHistory = styled.div`
   }
 `;
 
-const CurrentText = styled.span`
+const HistoryTitle = styled.span`
   width: 100%;
   height: 10%;
   display: flex;
